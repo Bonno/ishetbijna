0e8351f45b12c3fe3ae5a3c6459bb1877445d083
Bonno <GitHub@bonno.bo-ma.nl>
2015-06-22 19:35:25 +0000

    +: Use Twig as template engine

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-require  __DIR__ . '/lib/medoo.php';
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
+
+define('DOCROOT', __DIR__);
+
+require_once DOCROOT . '/vendor/autoload.php';
+require  DOCROOT . '/lib/medoo.php';
  
 $database = new medoo(array(
 	// required
@@ -22,76 +29,9 @@ if ($_SERVER['REQUEST_URI'] != '/') {
         'request' => $_SERVER['REQUEST_URI']
     ));
 }
- 
-?><html>
-<head>
-  <title>Is het bijna ...?</title>
 
-  <link rel="shortcut icon" href="http://ishetbijna.nl/favicon.ico">
-
-  <!-- Latest compiled and minified CSS -->
-  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"> -->
-
-  <!-- Optional theme -->
-  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css"> -->
-
-  <!-- Latest compiled and minified JavaScript -->
-  <script async src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-
-  <style>
-    html, body, #map-canvas {
-      height: 100%;
-      margin: 0px;
-      padding: 0px
-    }
-    body {text-align: center;}
-
-    a#answer {
-      display: inline-block;
-      margin-top: 200px;
-      font-weight: bold;
-      font-size: 120pt;
-      font-family: Arial, sans-serif;
-      text-decoration: none;
-      color: black;
-    }
-  </style>
-
-  <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
-  <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
-  <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
-  <link rel="icon" type="image/png" href="/favicon-194x194.png" sizes="194x194">
-  <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
-  <link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192">
-  <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
-  <link rel="manifest" href="/manifest.json">
-  <meta name="msapplication-TileColor" content="#00aba9">
-  <meta name="msapplication-TileImage" content="/mstile-144x144.png">
-  <meta name="theme-color" content="#c0c0c0">
-
-</head>
-<body>
-  <a id="answer"
-    href="http://ishetbijna.nl/live"
-    title="Is het bijna Live?">
-    NEE
-  </a>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-64171457-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
-</body>
-</html>
+$loader = new Twig_Loader_Filesystem(DOCROOT . '/templates');
+$twig = new Twig_Environment($loader, array(
+    'cache' => DOCROOT . '/cache',
+));
+echo $twig->render('index.phtml', array('name' => 'Fabien'));
